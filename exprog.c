af14298e91b3d59801ab2c3486928383ede96911
byllgrim <robin.a.t.pedersen@gmail.com>
2018-05-25 09:06:07 +0200

    extract cli arguments

--- exprog.c
+++ exprog.c
@@ -1,5 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+usage(char *prog)
+{
+    fprintf(stderr, "usage: %s\n", prog);
+}
+
+void
+die(char *msg)
+{
+    fprintf(stderr, msg);
+    exit(EXIT_FAILURE);
+}
+
 int
 main(int argc, char **argv)
 {
-	return 0;
+    int i;
+    int j;
+
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            usage(argv[0]);
+            die("");
+        }
+
+        for (j = 1; argv[i][j]; j++)
+            printf("%c\n", argv[i][j]);
+    }
+
+    return EXIT_SUCCESS;
 }
